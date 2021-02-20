bc64e9aa970cb2886b09e09854e79e4266a1a4c5
Gökmen Görgen <gkmngrgn@gmail.com>
2021-02-20 21:52:06 +0100

    fix project name.

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
   t.window.width = 512
   t.window.height = 512
-  t.window.title = "Eric"         -- The window title (string)
+  t.window.title = "Erik"         -- The window title (string)
 end
