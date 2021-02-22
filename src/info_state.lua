da658ff249f76a39ef2aefd7b287b6daefa9086d
Gökmen Görgen <gkmngrgn@gmail.com>
2021-02-22 18:02:06 +0100

    update opening text.

--- src/info_state.lua
+++ src/info_state.lua
@@ -5,9 +5,17 @@ local info_state = {
         self.font = love.graphics.newFont("assets/1980_font.ttf", 40)
         love.graphics.setFont(self.font)
 
-        self.opening_text = "NARRATIVE TEXT"
+        self.opening_text = [[
+ I am Defne. My dad thinks I eat a lot
+   of chocolate, and he didn't buy me
+chocolate today. However, I did not eat
+   them every day! I remember that I
+stacked some chocolates in my room. Now
+ help me to find the chocolates in my
+          room full of toys.
+        ]]
 
-        Timer.after(2, function()
+        Timer.after(10, function()
             sm:setState("game_state")
         end)
     end,
@@ -17,10 +25,9 @@ local info_state = {
 
     draw = function(self, dt, alpha)
         love.graphics.clear(43 / 255, 40 / 255, 33 / 255, 1)
-
         love.graphics.setColor(121 / 255, 121 / 255, 121 / 255, alpha)
         love.graphics.print(self.opening_text, (680 - self.font:getWidth(self.opening_text)) / 2,
-            (680 - self.font:getHeight(self.opening_text)) / 2)
+            (680 - self.font:getHeight(self.opening_text)) / 3)
     end,
 
     keypressed = function(self, key)
